afbe991fcb37e575cc9c2a21b1f14819293207a9
Devraj Vaishnav <devrajv2023@gmail.com>
2025-03-25 00:46:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+// Routes
+app.get("/test", (req,res)=>{
+ res.json({
+  message:"Hello Devraj",
+
+ })
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`✅ Server is running on port: ${port}`);
+});
